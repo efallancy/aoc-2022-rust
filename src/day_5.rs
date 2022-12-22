d61266f148aa812b01c584c6c9f5b8f4e149f77b
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2022-12-23 00:07:24 +1100

    refactor(day_5): Group with struct and Impl

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,71 +1,90 @@
 use crate::helper;
 use std::collections::HashMap;
 
-fn position_as_map(position: &str) -> HashMap<usize, Vec<String>> {
-    let mut positions = position.lines().collect::<Vec<&str>>();
+#[derive(Debug)]
+struct StackPosition {
+    position: HashMap<usize, Vec<String>>,
+}
 
-    positions.reverse();
+impl StackPosition {
+    fn new(position: &str) -> StackPosition {
+        let mut positions = position.lines().collect::<Vec<&str>>();
 
-    let mut map: HashMap<usize, Vec<String>> = HashMap::new();
+        positions.reverse();
 
-    for (i, position) in positions.iter().enumerate() {
-        let split_chars: Vec<char> = position.chars().collect();
-        let items: Vec<String> = split_chars
-            .chunks(4)
-            .map(|x| x.get(1).unwrap().to_string())
-            .collect();
-        if i == 0 {
-            for lane in items {
-                map.insert(lane.parse::<usize>().unwrap(), Vec::new());
-            }
-        } else {
-            for (i, item) in items.iter().enumerate() {
-                if item != " " {
-                    let key = i + 1;
-                    let mut value = map.get(&key).unwrap().to_owned();
-                    value.push(item.to_owned());
-                    map.insert(i + 1, value);
+        let mut map: HashMap<usize, Vec<String>> = HashMap::new();
+
+        for (i, position) in positions.iter().enumerate() {
+            let split_chars: Vec<char> = position.chars().collect();
+            let items: Vec<String> = split_chars
+                .chunks(4)
+                .map(|x| x.get(1).unwrap().to_string())
+                .collect();
+            if i == 0 {
+                for lane in items {
+                    map.insert(lane.parse::<usize>().unwrap(), Vec::new());
+                }
+            } else {
+                for (i, item) in items.iter().enumerate() {
+                    if item != " " {
+                        let key = i + 1;
+                        let mut value = map.get(&key).unwrap().to_owned();
+                        value.push(item.to_owned());
+                        map.insert(i + 1, value);
+                    }
                 }
             }
         }
+
+        StackPosition { position: map }
     }
 
-    map
-}
+    fn _move_items(&mut self, from: usize, to: usize, total_items: usize, as_stack: bool) {
+        let origin_value = self.position.get(&from).unwrap();
+        let destination_value = self.position.get(&to).unwrap();
 
-fn move_items(map: &mut HashMap<usize, Vec<String>>, from: usize, to: usize, total_items: usize) {
-    let origin_value = map.get(&from).unwrap();
-    let destination_value = map.get(&to).unwrap();
+        let range_to = origin_value.len();
+        let range_from = range_to - total_items;
 
-    let range_to = origin_value.len();
-    let range_from = range_to - total_items;
+        let to_move = &mut origin_value[range_from..range_to].to_vec().to_owned();
 
-    let to_move = &mut origin_value[range_from..range_to].to_vec().to_owned();
-    to_move.reverse();
+        if as_stack {
+            to_move.reverse();
+        }
 
-    let to_keep = &origin_value[..range_from].to_vec();
+        let to_keep = &origin_value[..range_from].to_vec();
 
-    let destination_new_value = [destination_value.to_owned(), to_move.to_owned()].concat();
+        let destination_new_value = [destination_value.to_owned(), to_move.to_owned()].concat();
 
-    map.insert(from, to_keep.to_owned());
-    map.insert(to, destination_new_value);
-}
+        self.position.insert(from, to_keep.to_owned());
+        self.position.insert(to, destination_new_value);
+    }
 
-fn move_items_as_is(map: &mut HashMap<usize, Vec<String>>, from: usize, to: usize, total_items: usize) {
-    let origin_value = map.get(&from).unwrap();
-    let destination_value = map.get(&to).unwrap();
+    fn move_items_as_stack(&mut self, from: usize, to: usize, total_items: usize) {
+        self._move_items(from, to, total_items, true);
+    }
 
-    let range_to = origin_value.len();
-    let range_from = range_to - total_items;
+    fn move_items_as_is(&mut self, from: usize, to: usize, total_items: usize) {
+        self._move_items(from, to, total_items, false);
+    }
 
-    let to_move = &mut origin_value[range_from..range_to].to_vec().to_owned();
+    fn get_final_elements_result(&self) -> String {
+        let total_columns = self.position.keys().count();
+        let mut final_elements: Vec<&str> = vec![];
 
-    let to_keep = &origin_value[..range_from].to_vec();
+        for column in (1..=total_columns).collect::<Vec<usize>>() {
+            match self.position.get(&column).unwrap().last() {
+                Some(v) => {
+                    final_elements.push(v);
+                }
+                _ => {
+                    final_elements.push(" ");
+                }
+            }
+        }
 
-    let destination_new_value = [destination_value.to_owned(), to_move.to_owned()].concat();
-
-    map.insert(from, to_keep.to_owned());
-    map.insert(to, destination_new_value);
+        final_elements.join("")
+    }
 }
 
 #[derive(Debug)]
@@ -88,24 +107,6 @@ fn parsed_instruction(instruction: &str) -> Instruction {
     }
 }
 
-fn print_result(map: &HashMap<usize, Vec<String>>) -> String {
-    let total_columns = map.keys().count();
-    let mut final_elements: Vec<&str> = vec![];
-
-    for column in (1..=total_columns).collect::<Vec<usize>>() {
-        match map.get(&column).unwrap().last() {
-            Some(v) => {
-                final_elements.push(v);
-            }
-            _ => {
-                final_elements.push(" ");
-            }
-        }
-    }
-
-    final_elements.join("")
-}
-
 pub fn solve() {
     let input = helper::read_input_day(5);
 
@@ -113,29 +114,19 @@ pub fn solve() {
     let position = content.first().unwrap().to_owned();
     let instructions = content.last().unwrap().to_owned();
 
-    let mut position_map_part_1 = position_as_map(position);
-    let mut position_map_part_2 = position_as_map(position);
+    let mut position_map_part_1 = StackPosition::new(position);
+    let mut position_map_part_2 = StackPosition::new(position);
 
     for instruction in instructions.lines().collect::<Vec<&str>>() {
         let parsed = parsed_instruction(instruction);
-        move_items(
-            &mut position_map_part_1,
-            parsed.from,
-            parsed.to,
-            parsed.total_items,
-        );
+        position_map_part_1.move_items_as_stack(parsed.from, parsed.to, parsed.total_items);
     }
 
     for instruction in instructions.lines().collect::<Vec<&str>>() {
         let parsed = parsed_instruction(instruction);
-        move_items_as_is(
-            &mut position_map_part_2,
-            parsed.from,
-            parsed.to,
-            parsed.total_items,
-        );
+        position_map_part_2.move_items_as_is(parsed.from, parsed.to, parsed.total_items);
     }
 
-    println!("Day 5 - Part 1: {:?}", print_result(&position_map_part_1)); // QNNTGTPFN
-    println!("Day 5 - Part 2: {:?}", print_result(&position_map_part_2)); // GGNPJBTTR
+    println!("Day 5 - Part 1: {:?}", position_map_part_1.get_final_elements_result()); // QNNTGTPFN
+    println!("Day 5 - Part 2: {:?}", position_map_part_2.get_final_elements_result()); // GGNPJBTTR
 }
